10497074cf220df791d8da4553569218173fa460
didisouzacosta <adrianosouzacostaios@gmail.com>
2021-03-16 08:56:34 -0300

    feat: simplificando assintura do protocolo FileArchive

--- ios/Adapters/Compress.swift
+++ ios/Adapters/Compress.swift
@@ -9,27 +9,11 @@ import Foundation
 
 public protocol Compress {
     
-    static var type: Type { get }
-    
     static func compress(
         _ filePath: String,
         to destination: String,
         password: String?,
         progressHandler: ((Double) -> Void)?
     ) throws
-    
-}
-
-extension Compress {
-    
-    static func destinationPath(
-        filePath: String,
-        destination: String
-    ) -> String {
-        let lastPath = (filePath as NSString).lastPathComponent
-        let pathPieces = lastPath.split(separator: ".")
-        
-        return destination + pathPieces[0] + ".\(type.rawValue)"
-    }
     
 }
--- ios/Adapters/FileArchive.swift
+++ ios/Adapters/FileArchive.swift
@@ -7,4 +7,22 @@
 
 import Foundation
 
-public typealias FileArchive = Compress & Decompress
+public protocol FileArchive: Compress & Decompress {
+    
+    static var type: Type { get }
+    
+}
+
+extension FileArchive {
+    
+    static func destinationPath(
+        filePath: String,
+        destination: String
+    ) -> String {
+        let lastPath = (filePath as NSString).lastPathComponent
+        let pathPieces = lastPath.split(separator: ".")
+        
+        return destination + pathPieces[0] + ".\(type.rawValue)"
+    }
+    
+}
