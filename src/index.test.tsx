be420fb024d02fa67613d9f027497379ebd24c90
didisouzacosta <adrianosouzacostaios@gmail.com>
2021-04-23 08:08:08 -0300

    chore: unit testes react native layer to isProtected method

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -3,6 +3,7 @@ jest.mock('react-native', () => {
     NativeModules: {
       Uncompress: {
         extract: jest.fn(),
+        isProtected: jest.fn(),
       },
     },
   };
@@ -16,45 +17,89 @@ describe('Uncompress', () => {
     jest.resetAllMocks();
   });
 
-  it('ensure default values', async () => {
-    const extractMock = jest.spyOn(NativeModules.Uncompress, 'extract');
+  describe('Extract', () => {
+    it('ensure default values', async () => {
+      const extractMock = jest.spyOn(NativeModules.Uncompress, 'extract');
 
-    await Uncompress.extract({
-      filePath: 'test_file.cbz',
-      destination: 'temp',
-    });
-
-    expect(extractMock).toBeCalledWith(
-      'test_file.cbz',
-      'temp',
-      true,
-      undefined
-    );
-  });
-
-  it('should decompress files if extract successful', async () => {
-    const extractMock = jest.spyOn(NativeModules.Uncompress, 'extract');
-
-    await Uncompress.extract({
-      filePath: 'test_file.cbz',
-      destination: 'temp',
-      overwrite: true,
-      password: '123',
-    });
-
-    expect(extractMock).toBeCalledTimes(1);
-    expect(extractMock).toBeCalledWith('test_file.cbz', 'temp', true, '123');
-  });
-
-  it('throw error if extract fails', async () => {
-    const extractMock = jest.spyOn(NativeModules.Uncompress, 'extract');
-    extractMock.mockRejectedValueOnce('Simulation Error');
-
-    await expect(
-      Uncompress.extract({
+      await Uncompress.extract({
         filePath: 'test_file.cbz',
         destination: 'temp',
-      })
-    ).rejects.toEqual('Simulation Error');
+      });
+
+      expect(extractMock).toBeCalledWith(
+        'test_file.cbz',
+        'temp',
+        true,
+        undefined
+      );
+    });
+
+    it('should decompress files if extract successful', async () => {
+      const extractMock = jest.spyOn(NativeModules.Uncompress, 'extract');
+
+      await Uncompress.extract({
+        filePath: 'test_file.cbz',
+        destination: 'temp',
+        overwrite: true,
+        password: '123',
+      });
+
+      expect(extractMock).toBeCalledTimes(1);
+      expect(extractMock).toBeCalledWith('test_file.cbz', 'temp', true, '123');
+    });
+
+    it('throw error if extract fails', async () => {
+      const extractMock = jest.spyOn(NativeModules.Uncompress, 'extract');
+      extractMock.mockRejectedValueOnce('Simulation Error');
+
+      await expect(
+        Uncompress.extract({
+          filePath: 'test_file.cbz',
+          destination: 'temp',
+        })
+      ).rejects.toEqual('Simulation Error');
+    });
+  });
+
+  describe('IsProtected', () => {
+    it('Return true if the file is protected', async () => {
+      const isProtectedMock = jest.spyOn(
+        NativeModules.Uncompress,
+        'isProtected'
+      );
+      isProtectedMock.mockResolvedValueOnce(true);
+
+      const isProtected = await Uncompress.isProtected({
+        filePath: 'teste_file.cbz',
+      });
+
+      expect(isProtected).toBe(true);
+    });
+
+    it('Return false if the file is not protected', async () => {
+      const isProtectedMock = jest.spyOn(
+        NativeModules.Uncompress,
+        'isProtected'
+      );
+      isProtectedMock.mockResolvedValueOnce(false);
+
+      const isProtected = await Uncompress.isProtected({
+        filePath: 'teste_file.cbz',
+      });
+
+      expect(isProtected).toBe(false);
+    });
+
+    it('Throw error is isProtected fails', async () => {
+      const isProtectedMock = jest.spyOn(
+        NativeModules.Uncompress,
+        'isProtected'
+      );
+      isProtectedMock.mockRejectedValueOnce('Simulation error');
+
+      await expect(
+        Uncompress.isProtected({ filePath: 'test_file.cbz' })
+      ).rejects.toEqual('Simulation error');
+    });
   });
 });
--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,31 @@ import { NativeModules } from 'react-native';
 
 const { Uncompress } = NativeModules;
 
+type ExtractParams = {
+  filePath: string;
+  destination: string;
+  overwrite?: boolean;
+  password?: string;
+};
+
+type IsProtectedParams = Pick<ExtractParams, 'filePath'>;
+
 const extract = async ({
   filePath,
   destination,
   overwrite = true,
   password = undefined,
-}: {
-  filePath: string;
-  destination: string;
-  overwrite?: boolean;
-  password?: string;
-}) => {
+}: ExtractParams) => {
   await Uncompress.extract(filePath, destination, overwrite, password);
+};
+
+const isProtected = async ({
+  filePath,
+}: IsProtectedParams): Promise<boolean> => {
+  return await Uncompress.isProtected(filePath);
 };
 
 export default {
   extract,
+  isProtected,
 };
